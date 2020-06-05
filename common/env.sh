072612973fdedc77c8d2ceaf1c17d47face2ea3f
wuren <wuren@dtstack.com>
2020-06-05 21:11:23 +0800

    add component stack.yml

---
+++ common/env.sh
@@ -0,0 +1,7 @@
+OPT_HOME=/Users/luna/opt
+
+export MAVEN_HOME=${OPT_HOME}/maven
+export KAFKA_HOME=${OPT_HOME}/kafka
+export SCALA_HOME=${OPT_HOME}/scala
+
+export PATH=${PATH}:${MAVEN_HOME}/bin:${SCALA_HOME}/bin:${KAFKA_HOME}/bin
